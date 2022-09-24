c573241d08149a426d81fbb62993fdfa6e1ea507
Richard Mbabie <richardmbabie@gmail.com>
2022-09-24 18:01:33 +0100

    changed the host address

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3f3ebfad1b7043f08c32157dba54de3-1710139159.us-west-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
